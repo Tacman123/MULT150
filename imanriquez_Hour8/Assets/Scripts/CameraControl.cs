39fce2f3766c593b65f4bcc7c2d9b3b4c2823e9a
Tacman123 <142944174+Tacman123@users.noreply.github.com>
2024-04-08 22:18:05 -0700

    Hour 8 Commit

---
+++ imanriquez_Hour8/Assets/Scripts/CameraControl.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mxVal = Input.GetAxis("Mouse X");
+        float myVal = Input.GetAxis("Mouse Y");
+
+        if (myVal != 0)
+        {
+            transform.Rotate(0f - myVal, 0f, 0f);
+        }
+        if (mxVal != 0)
+        {
+            transform.Rotate(0f, 0f + mxVal, 0f);
+        } 
+    }
+}
