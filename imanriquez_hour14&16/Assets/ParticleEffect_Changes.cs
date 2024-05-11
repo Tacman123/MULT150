caf1c7b75ecad49df506964ba3c7da573b4952f5
Tacman123 <142944174+Tacman123@users.noreply.github.com>
2024-05-11 16:43:14 -0700

    Commiting Hour 14 and 16
    Very pleased with the results made.

---
+++ imanriquez_hour14&16/Assets/ParticleEffect_Changes.cs
@@ -0,0 +1,9 @@
+// I have made changes to the original particle system to as recreate the flame prefab while still keeping the vertical emission of the flame
+// I have changed the sprite in the render nodde to the flame sprite sheet as well as activated the "Texture Sheet Animation" and adjust it the tiles to read the sprite sheet accurately
+// The "Color Over Lifeime" and "Size over Lifetime" modules both were activated. 
+// I changed the colors to start at an orange transition to red, then blue, and end on green all the while changing size from the original size slightly billow out much larger then finally to decay to a wisp at the top of the flame emitter.
+// The shape was modified to more suit the cone shape i wanted the flame to be emitted out like.
+// A translucent trail was added to each particle to give it a bit of a cartoon like fantasy look to offset the more realistic looking fire.
+// All changes made to the particle effect settings was:
+// Duration, Start Lifetime, gravity modifier, Simulation Space, *Delta Time, Max Particles.
+// Overall happy with the look of it.
