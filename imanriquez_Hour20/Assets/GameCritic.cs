8888ab3fe349058927a53d240d38a09085a192c5
Tacman123 <142944174+Tacman123@users.noreply.github.com>
2024-05-20 17:06:59 -0700

    Pushing my Hour 20
    Please let me know if you want me to implement the difficulty changes to the game.

---
+++ imanriquez_Hour20/Assets/GameCritic.cs
@@ -0,0 +1,18 @@
+/* This game was not that difficult and is actually quit fair.
+ * The speed at which everything runs towards you is actually quite well put together
+ * The speed of the player strafing is very well balanced. 
+ * The game is so balanced that I forget I even have the :Phase Out" ability
+ * If I were to make the game more difficult, I would change the speed of the obstacle hit penalty. The speed should be sped up not slowed.
+ * Slowing down the world also effects the slowing of the in-game timer as well.
+ * If the player was sped up yes time powerups would fly at the player but so will the obstacles.
+ * In order to counter the speed, I would make the player phase for a temporary amount of time.
+ * THe wow factor I would like to add would be to lean into the "Android" main character and the dystopian cyperpunk look to the environment. 
+ * I've gone ahead and changed the obstacles to some robots running towards the player.
+ * I think those changes add to the "Wow" factor of the game.
+ * The game's challenge of trying to go as long as you can while continuing to dodge obstacles is a testament to games like Temple Run. The aeshthetic of the game is very pleasing as well.
+ * The game is only tedious in the regards of the character feeling they are running slow while everything else speeds past
+ * It also is feels kind of bad when you need an upgrade or try to grab an powerup but obstacles spawn practically on top of them.
+ * The obstacles were changed to a robot with a running animation. They had a directional issue, but I've went ahead and fixed that by changing the local rotation of the both the robot and power ups and changing the code to positive movement speed.
+ * The powerup mesh was changed and to a cube and enlarged, with clock hands added to it to give the look of a squared clock.
+ * The material was also changed to a white slightly transparent color.
+ * I then also changed the particle system a slightly lengthened particles with a a skinny ribbon to make it look important and like a powerup. */
